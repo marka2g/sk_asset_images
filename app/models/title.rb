257af858953638894677e9146346046308ee69b9
Mark Sadegi <mark.sadegi@gmail.com>
2013-06-27 06:56:41 -0500

    fixed AssetUploader typo

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,5 +1,5 @@
 class Title < ActiveRecord::Base
-  mount_uploader :asset_image, AvatarUploader
+  mount_uploader :asset_image, AssetImageUploader
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
   attr_accessible :name, :asset_image, :crop_x, :crop_y, :crop_w, :crop_h
   after_update :crop_asset_image
