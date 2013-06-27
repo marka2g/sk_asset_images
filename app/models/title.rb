0c917b7ae1839d9282ea348f5079d0ff6d06d9f7
Mark Sadegi <mark.sadegi@gmail.com>
2013-06-27 09:51:56 -0500

    replaced with haml

--- app/models/title.rb
+++ app/models/title.rb
@@ -4,7 +4,7 @@ class Title < ActiveRecord::Base
   attr_accessible :name, :asset_image, :crop_x, :crop_y, :crop_w, :crop_h
   after_update :crop_asset_image
 
-  # validates :name, :presence => true, :uniqueness => {:case_sensitive => false}
+  validates :name, :presence => true, :uniqueness => {:case_sensitive => false}
 
   def crop_asset_image
     asset_image.recreate_versions! if crop_x.present?
