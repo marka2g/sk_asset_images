b999595c50f767d049fbadcf7a66de6243e001b8
Mark Sadegi <mark.sadegi@gmail.com>
2013-06-28 11:06:22 -0500

    cleaning up to create a base branch

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,12 +1,5 @@
 class Title < ActiveRecord::Base
-  mount_uploader :asset_image, AssetImageUploader
-  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
-  attr_accessible :name, :asset_image, :crop_x, :crop_y, :crop_w, :crop_h
-  after_update :crop_asset_image
+  attr_accessible :name
 
   validates :name, :presence => true, :uniqueness => {:case_sensitive => false}
-
-  def crop_asset_image
-    asset_image.recreate_versions! if crop_x.present?
-  end
 end
