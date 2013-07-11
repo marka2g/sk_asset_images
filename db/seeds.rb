d938f76bc8ef0a012340b10dc3d5b7e33f522f7e
Mark Sadegi <mark.sadegi@gmail.com>
2013-07-10 21:19:59 -0500

    little bit a cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-# setup - refactor/cleanup, yuk!
-def grab_test_files(id)
+# verbose setup
+def grab_test_file(id)
 	path = "#{Rails.root.join('public/test_images')}/#{id}/"
 	file_name = Dir.entries("#{path}").last
 	the_file = File.open(path + file_name)
@@ -21,42 +21,42 @@ Title.create(name: 'Game of Thrones')
 
 c1 = Celeb.where(name: "Kevin Spacey").first
 new_image_c1 = Image.create!(attachable_id: c1.id, attachable_type: c1.class.name)
-test_file_c1 = grab_test_files(new_image_c1.attachable_id)
+test_file_c1 = grab_test_file(new_image_c1.attachable_id)
 new_image_c1.filename.store!(test_file_c1)
 c1.images << new_image_c1
 c1.save!
 
 c2 = Celeb.where(name: "Keanu Reeves").first
 new_image_c2 = Image.create!(attachable_id: c2.id, attachable_type: c2.class.name)
-test_file_c2 = grab_test_files(new_image_c2.attachable_id)
+test_file_c2 = grab_test_file(new_image_c2.attachable_id)
 new_image_c2.filename.store!(test_file_c2)
 c2.images << new_image_c2
 c2.save!
 
 c3 = Celeb.where(name: "Bob Marley").first
 new_image_c3 = Image.create!(attachable_id: c3.id, attachable_type: c3.class.name)
-test_file_c3 = grab_test_files(new_image_c3.attachable_id)
+test_file_c3 = grab_test_file(new_image_c3.attachable_id)
 new_image_c3.filename.store!(test_file_c3)
 c3.images << new_image_c3
 c3.save!
 
 t1 = Title.where(name: "Breaking Bad").first
 new_image_t1 = Image.create!(attachable_id: t1.id, attachable_type: t1.class.name)
-test_file_t1 = grab_test_files(new_image_t1.attachable_id + 3) # + 3, lame...
+test_file_t1 = grab_test_file(new_image_t1.attachable_id + 3) # + 3, lame...
 new_image_t1.filename.store!(test_file_t1)
 t1.images << new_image_t1
 t1.save!
 
 t2 = Title.where(name: "Emmanuelle").first
 new_image_t2 = Image.create!(attachable_id: t2.id, attachable_type: t2.class.name)
-test_file_t2 = grab_test_files(new_image_t2.attachable_id + 3)
+test_file_t2 = grab_test_file(new_image_t2.attachable_id + 3)
 new_image_t2.filename.store!(test_file_t2)
 t2.images << new_image_t2
 t2.save!
 
 t3 = Title.where(name: "Game of Thrones").first
 new_image_t3 = Image.create!(attachable_id: t3.id, attachable_type: t3.class.name)
-test_file_t3 = grab_test_files(new_image_t3.attachable_id + 3)
+test_file_t3 = grab_test_file(new_image_t3.attachable_id + 3)
 new_image_t3.filename.store!(test_file_t3)
 t3.images << new_image_t3
 t3.save!
