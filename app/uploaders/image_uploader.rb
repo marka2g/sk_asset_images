b3b877d5c744e850131c29d18f25f4f9ffa8c2e9
Mark Sadegi <mark.sadegi@gmail.com>
2013-07-16 08:17:38 -0500

    adding method missing and define_method in carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,50 +1,37 @@
 # encoding: utf-8
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
-  before :store, :setup_available_image_types
-  # after :store, :setup_available_image_types
+
+  before :cache, :setup_available_sizes
+  # before :store, :setup_available_sizes
 
   storage :file
-
-  # def initialize(model = nil, mounted_as = nil)
-binding.pry
-    if @image_types
-      puts 'yo'
-      # @image_types.each do |type|
-      #   version type.name.to_sym do
-      #     process :resize_to_fill => [type.crop_x, type.crop_y]
-      #   end
-      # end
-    end
-  # end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  # this works because version is defined
+  # ImageType.all.each do |type|
+  #   version type.name.to_sym do
+  #     process :resize_to_fill => [type.crop_x, type.crop_y]
+  #   end
+  # end
+
   def method_missing(method, *args)
-    # return false if method.to_s.match(/has_(.*)_size\?/)
+    return false if method.to_s.match(/not_su/)
     super
   end
 
-protected
-
-  def setup_available_image_types(file)
-binding.pry
-    @image_types = []
- # binding.pry
-    model.attachable.image_types.each do |type|
-      @image_types << type   
-     # self.class_eval do
-     #  version type.name.to_sym do
-     #    process :resize_to_fill => [type.crop_x, type.crop_y]
-     #  end
-     # end
-     return @image_types
+  protected
+  # the method called at the start
+  def setup_available_sizes(file)
+    model.attachable.image_types.each do |type|      
+      self.class_eval do
+        define_method("#{type.name}".to_sym) { true }
+      end
     end
   end
 
-
 end
-
 
